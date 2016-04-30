f963e2f6fbf5a2dfe7be4db85596dc65518fc28d
Hadouweb <kengo14@hotmail.fr>
2016-04-30 15:32:09 +0200

    Besoin d'apprendre c# lol

--- skynet.cs
+++ skynet.cs
@@ -86,4 +86,74 @@ class Player
             Console.WriteLine("0 1");
         }
     }
+
+    public class Node<T>
+    {
+        // Private member-variables
+        private T data;
+        private NodeList<T> neighbors = null;
+
+        public Node() {}
+        public Node(T data) : this(data, null) {}
+        public Node(T data, NodeList<T> neighbors)
+        {
+            this.data = data;
+            this.neighbors = neighbors;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+            }
+        }
+
+        protected NodeList<T> Neighbors
+        {
+            get
+            {
+                return neighbors;
+            }
+            set
+            {
+                neighbors = value;
+            }
+        }
+    }
+
+    public class GraphNode<T> : Node<T>
+    {
+        private List<int> costs;
+
+        public GraphNode() : base() { }
+        public GraphNode(T value) : base(value) { }
+        public GraphNode(T value, NodeList<T> neighbors) : base(value, neighbors) { }
+
+        new public NodeList<T> Neighbors
+        {
+            get
+            {
+                if (base.Neighbors == null)
+                    base.Neighbors = new NodeList<T>();
+
+                return base.Neighbors;
+            }            
+        }
+
+        public List<int> Costs
+        {
+            get
+            {
+                if (costs == null)
+                    costs = new List<int>();
+
+                return costs;
+            }
+        }
+    }
 }
