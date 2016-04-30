1afa3b9809dc7d31627c2508937100ff57e1e4fd
hadouweb <kengo14@hotmail.fr>
2016-04-30 20:16:49 +0200

    v2

--- chal/chal.cs
+++ chal/chal.cs
@@ -46,18 +46,57 @@ class Player
             }
             //colors.ForEach(color => Console.Error.WriteLine("ColorA {0}, ColorB : {1}", color.ColorA, color.ColorB));
             Color c = colors.First();
-            int col = GetBestCol(map, c);
-            //Console.Error.WriteLine("Col resul : {0}", col);
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    //Console.Error.WriteLine("TEST {0} {1}", c.ColorA, map[i][j]);
+                    Console.Error.Write(map[i][j]);
+                }
+                Console.Error.WriteLine();
+            }
+            int col = GetHeightCol(map, c);
+            Console.Error.WriteLine("Col resul : {0}", col);
             Console.WriteLine(col); // "x": the column in which to drop your blocks
             colors.RemoveAt(0);
         }
     }
 
-    static int GetBestCol(char[][] map, Color c)
+    static char GetFirstHeightPos(char[][] map, int j)
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            if (map[i][j] != '.' && map[i][j] != '0')
+                return map[i][j];
+        }
+        return 'e';
+    }
+
+    static int GetHeightCol(char[][] map, Color c)
     {
         int     col = 0;
-
         //Console.Error.WriteLine("ColorA {0}, ColorB : {1}", c.ColorA, c.ColorB);
+        for (int j = 0; j < 6; j++)
+        {
+            char  firstChar = GetFirstHeightPos(map, j);
+            Console.Error.WriteLine("firstChar : {0}", firstChar);
+            //Console.Error.WriteLine("TEST {0} {1}", c.ColorA, map[i][j]);
+            if (c.ColorA == firstChar)
+            {
+                //Console.Error.WriteLine("TEST");
+                return j;
+            }
+            //Console.Error.Write(map[i][j]);
+        }
+        col = GetWidthCol(map, c);
+        return col;
+    }
+
+    static int GetWidthCol(char[][] map, Color c)
+    {
+        int     minCol = 12;
+        int     col = 0;
+
         for (int i = 0; i < 12; i++)
         {
             for (int j = 0; j < 6; j++)
@@ -65,8 +104,10 @@ class Player
                 //Console.Error.WriteLine("TEST {0} {1}", c.ColorA, map[i][j]);
                 if (c.ColorA == map[i][j])
                 {
-                    //Console.Error.WriteLine("TEST");
-                    return j;
+                    if (j - 1 > 0 && map[i][j - 1] == '.')
+                        return j - 1;
+                    if (j + 1 < 6 && map[i][j + 1] == '.')
+                        return j + 1;
                 }
                 //Console.Error.Write(map[i][j]);
             }
@@ -79,7 +120,7 @@ class Player
     static int GetMinCol(char[][] map, Color c)
     {
         int     minCol = 12;
-        int     result = 0;
+        int     col = 0;
 
         for (int i = 11; i >= 0; i--)
         {
@@ -92,6 +133,6 @@ class Player
                 }
             }
         }
-        return result;
+        return col;
     }
 }
